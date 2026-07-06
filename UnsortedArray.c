860f7797b7ab01973f7018ebaf848f50a72934de
Srinu-Mylapalli <srinivasmylapali@gmail.com>
2026-07-06 22:45:42 +0530

    Add unsorted array implementation with insertion, deletion, and traversal
    Implement basic operations for an unsorted array in C.

---
+++ UnsortedArray.c
@@ -0,0 +1,108 @@
+//to implement unsorted array using C
+#include <stdio.h>
+#define capacity 5
+
+int A[capacity], n = 0;
+
+// function prototype
+void insertion(int);
+void deletion(int);
+void traverse();
+
+int main()
+{
+    int ch, ele;
+    char c;
+
+    do
+    {
+        printf("Unsorted Array operations");
+        printf("\n1. Insertion\n");
+        printf("2. Deletion\n");
+        printf("3. Traverse\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            printf("Enter the element to insert: ");
+            scanf("%d", &ele);
+            insertion(ele);
+            break;
+
+        case 2:
+            printf("Enter the element to delete: ");
+            scanf("%d", &ele);
+            deletion(ele);
+            break;
+
+        case 3:
+            traverse();
+            break;
+
+        default:
+            printf("Invalid choice");
+        }
+
+        printf("\nDo you want to continue(y/n): ");
+        scanf(" %c", &c);
+    } while (c == 'y');
+
+    return 0;
+}
+
+void insertion(int ele)
+{
+    if (n == capacity)
+        printf("Array is full, insertion is not possible");
+    else
+    {
+        A[n] = ele;
+        n++;
+    }
+}
+
+void deletion(int ele)
+{
+    int pos = -1;
+    int i;
+
+    if (n == 0)
+    {
+        printf("No element in Array, deletion not possible");
+    }
+    else
+    {
+        for (i = 0; i <= n - 1; i++)
+        {
+            if (A[i] == ele)
+            {
+                pos = i;
+                break;
+            }
+        }
+
+        if (pos == -1)
+        {
+            printf("Given element not available in the Array, deletion not possible");
+        }
+        else
+        {
+            for (i = pos + 1; i <= n - 1; i++)
+            {
+                A[i - 1] = A[i];
+            }
+            n--;
+        }
+    }
+}
+
+void traverse()
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+}
